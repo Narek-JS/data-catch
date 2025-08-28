5b7638a927640d51026696438994c5317a5aed40
Narek Petrosyan <narek.petrosyan@softconstruct.com>
2025-08-28 18:59:24 +0400

    done the script to collect existed profile names

--- inspect-script/collectExistedProfileNames.js
+++ inspect-script/collectExistedProfileNames.js
@@ -1,6 +1,10 @@
 const methods = {
   redirectToNextProfilePage: () => {
     const profilesFromStorage = methods.storage.getProfiles();
+    if (profilesFromStorage.length === 0) {
+      return;
+    }
+
     const nextProfileUrl = profilesFromStorage.shift();
     methods.storage.setProfiles(profilesFromStorage);
 
@@ -35,36 +39,56 @@ function getProfileName() {
   return fullname;
 }
 
-async function saveProfileName({ fullname }) {
-  const currentProfileUrl = window.location.href;
+async function saveProfileName({ url, name }) {
+  const response = await fetch('http://localhost:3000/profiles', {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ url, name }),
+  });
 
+  if (!response.ok) {
+    console.error('Failed to save profile name.', await response.json());
+  } else {
+    console.log('Successfully saved.');
+  }
   // POST TO SAVE THIS FULLNAME FOR CURRENT PROFILE WHO HAVE THIS currentProfileUrl IN PROFILE URL.
 }
 
-async function getAllProfile(doneCb) {
+async function getProfilesToUpdate(doneCb) {
   const profilesFromStorage = methods.storage.getProfiles();
 
+  // If we still have profiles in our local queue, just run the callback.
   if (profilesFromStorage.length) {
     return doneCb();
   }
 
-  const url = 'http://localhost:3000/profiles';
+  const url = 'http://localhost:3000/profiles/needs-update?limit=10';
 
   try {
     const responseStream = await fetch(url);
-    const response = await responseStream.json();
+    const profiles = await responseStream.json();
 
-    methods.storage.setProfiles(response);
+    if (profiles.length === 0) {
+      console.log('No more profiles to update. All work is done!');
+      return;
+    }
+
+    const profileUrls = profiles.map((p) => p.url);
+    methods.storage.setProfiles(profileUrls);
     doneCb();
-  } catch {
-    console.log('something want wrong in profiles GET request');
+  } catch (error) {
+    console.error('Something went wrong fetching the profiles list:', error);
   }
 }
 
-getAllProfile(async () => {
-  const profileFullName = getProfileName();
-  if (profileFullName) {
-    await saveProfileName({ fullname: profileFullName });
+getProfilesToUpdate(async () => {
+  const profileName = getProfileName();
+  const currentUrl = window.location.href;
+
+  if (profileName) {
+    await saveProfileName({ url: currentUrl, name: profileName });
+  } else {
+    console.log('Could not find a name on this page.');
   }
 
   methods.redirectToNextProfilePage();
