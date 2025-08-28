943c9b002a0540d3a6d6fe24db4966516a729259
Narek Petrosyan <narek.petrosyan@softconstruct.com>
2025-08-28 16:12:20 +0400

    remove users enpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { UsersModule } from "./users/users.module";
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
@@ -15,17 +14,16 @@ import { ProfilesModule } from './profiles/profiles.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: "postgres",
-        host: configService.get<string>("DB_HOST"),
-        port: parseInt(configService.get<string>("DB_PORT") || "5432"),
-        username: configService.get<string>("DB_USERNAME"),
-        password: configService.get<string>("DB_PASSWORD"),
-        database: configService.get<string>("DB_DATABASE"),
-        entities: [__dirname + "/**/*.entity{.ts,.js}"],
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT') || '5432'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true, // DO NOT use in production - auto-creates DB tables
       }),
     }),
-    UsersModule,
     ProfilesModule,
   ],
   controllers: [AppController],
